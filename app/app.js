8db82872890888f5bd8c0dbfa1e4248c5dd29a37
yosiaagustadewa <yosiaagustadewa@gmail.com>
2020-10-15 11:07:38 +0700

    change

--- app/app.js
+++ app/app.js
@@ -2,17 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+let server = app.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}`);
+});
 
+server.keepAliveTimeout = 30000;
 
 app.get('/', (req, res) => {
     res.send('this is Home');
 })
 
 app.get('/insert:insert', (req, res) => {
-    console.log(req.params.insert);
+    console.log("inserting", req.params.insert);
     res.sendStatus(200);
 })
 
-app.listen(PORT, () => {
-    console.log(`Listening on Port ${PORT}`);
-});
+app.get('/do:job', (req, res) => {
+    console.log("doing", req.params.job);
+    res.send(`doing ${req.params.job}`);
+})
